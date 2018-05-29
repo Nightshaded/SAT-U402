d10ae973b1ab87ff7c89c86830ab7cc5932aa156
Nightshaded <39718963+Nightshaded@users.noreply.github.com>
2018-05-29 19:01:22 +1000

    Add files via upload

---
+++ Training Efficiency Calculator/Training Efficiency Calculator/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace Training_Efficiency_Calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.LargeTitle = new System.Windows.Forms.Label();
+            this.Subtitle = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(455, 940);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // LargeTitle
+            // 
+            this.LargeTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LargeTitle.Location = new System.Drawing.Point(109, 42);
+            this.LargeTitle.Name = "LargeTitle";
+            this.LargeTitle.Size = new System.Drawing.Size(870, 91);
+            this.LargeTitle.TabIndex = 1;
+            this.LargeTitle.Text = "Training Efficiency Calculator";
+            // 
+            // Subtitle
+            // 
+            this.Subtitle.AutoSize = true;
+            this.Subtitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Subtitle.Location = new System.Drawing.Point(30, 176);
+            this.Subtitle.Name = "Subtitle";
+            this.Subtitle.Size = new System.Drawing.Size(88, 42);
+            this.Subtitle.TabIndex = 2;
+            this.Subtitle.Text = "Title";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1095, 1155);
+            this.Controls.Add(this.Subtitle);
+            this.Controls.Add(this.LargeTitle);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label LargeTitle;
+        private System.Windows.Forms.Label Subtitle;
+    }
+}
+
